15bea9977d0d818be8b529598ed64156ce54ad9d
shahar <shachar@zilberman.org>
2017-11-19 09:27:31 +0200

    added stuff

--- js/main.js
+++ js/main.js
@@ -11,14 +11,17 @@ var gImgObjs = [
     { id: 7, name: 'One-Does-Not-Simply', url: 'img/memes/One-Does-Not-Simply.jpg', keywords: ['human', 'sean', 'bean', 'lord of the rings', 'boromir'] },
     { id: 8, name: 'Third-World-Skeptical-Kid', url: 'img/memes/Third-World-Skeptical-Kid.jpg', keywords: ['problem', 'human', 'black', 'african'] },
     { id: 9, name: 'X-Everywhere', url: 'img/memes/X-Everywhere.jpg', keywords: ['cartoon', 'buzz', 'woody', 'toy story'] },
+    { id: 10, name: 'Y-U-No', url: 'img/memes/Y-U-No.jpg', keywords: ['cartoon', 'angry'] },
 ]
 
 var gKeywords = {};
 
-function init() {
-    getKeywordsMap()    
-    renderImages(gImgObjs);
+var gUserPrefs = { imgId: 1, align: 'center', pos: 150, fillColor: 'white', fontSize: 30, font: 'Arial', shadowBlur: 0 }
 
+function init() {
+    getKeywordsMap()
+    renderImages(gImgObjs);
+    renderCanvas()
 }
 
 //This function takes the name of the img object and iserts it to the keywords
@@ -34,9 +37,6 @@ function expandKeywords() {
 
 expandKeywords();
 
-
-
-
 //This function renders the img objects and the popular keywords to the DOM
 function renderImages(ImgObjs) {
     expandKeywords();
@@ -45,7 +45,7 @@ function renderImages(ImgObjs) {
     ImgObjs.forEach(function stringToHtml(imgObj) {
         strHtml += `
         <li>
-        <img src="${imgObj.url}" alt="${imgObj.name}">
+        <img src="${imgObj.url}" alt="${imgObj.name}" class="img-${imgObj.id}" onclick="changeStep('step-two',${imgObj.id})"/>
         <p>${imgObj.name}</p>
         // <a href="lmgtfy/${imgObj.name}">About this meme</a>
     </li>`})
@@ -60,18 +60,131 @@ function renderImages(ImgObjs) {
 
 function getKeywordsMap() {
     gImgObjs.forEach(function (imgObj) {
-        var imgKeys = imgObj.keywords
+        var imgKeys = imgObj.keywords;
         imgKeys.forEach(function (key) {
-            if (!gKeywords[key]) gKeywords[key] = { count: 0, imgObjs: [] }
-            gKeywords[key].count++
-            gKeywords[key].imgObjs.push(imgObj)
+            if (!gKeywords[key]) gKeywords[key] = { count: 0, imgObjs: [] };
+            gKeywords[key].count++;
+            gKeywords[key].imgObjs.push(imgObj);
         })
     })
 }
 
-function filterBySearch(userKey) {
-    if (!userKey) var userKey = document.querySelector('.search').value
-    var filterdImgs = gKeywords[userKey].imgObjs
-    renderImages(filterdImgs)
+function filterBySearch(userKey, ev) {
+    if (ev) {
+        if (ev.keyCode !== 13) return;
+    }
+    if (!userKey) var userKey = document.querySelector('.search').value;
+    var filterdImgs = gKeywords[userKey].imgObjs;
+    renderImages(filterdImgs);
 }
 
+function changeStep(step, imgId) {
+    var step1 = document.querySelector('.step-one');
+    var elTopText = document.querySelector('.top-txt');
+    var elBottomText = document.querySelector('.bottom-txt');
+    elTopText.value = 'top meme text'
+    elBottomText.value = ''
+    if (step1.classList.contains('show') && !imgId) {
+        imgId = 10;
+        elTopText.value = 'y u no'
+        elBottomText.value = 'choose image?'
+    }
+    var elPrevStep = document.querySelector('.show');
+    elPrevStep.classList.remove('show')
+    var elCurrStep = document.querySelector('.' + step + '')
+    elCurrStep.classList.add('show')
+    gUserPrefs.imgId = imgId;
+    if (imgId) renderCanvas();
+}
+
+function renderCanvas() {
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var img = document.querySelector('.img-' + gUserPrefs.imgId + '');
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    ctx.font = gUserPrefs.fontSize + "px " + gUserPrefs.font;
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 4;
+    ctx.fillStyle = gUserPrefs.fillColor;
+    ctx.textAlign = gUserPrefs.align;
+    ctx.shadowColor = 'black'
+    ctx.shadowBlur = gUserPrefs.shadowBlur;
+    var topText = document.querySelector('.top-txt').value;
+    ctx.strokeText(topText, gUserPrefs.pos, 50);
+    ctx.fillText(topText, gUserPrefs.pos, 50);
+    var bottomText = document.querySelector('.bottom-txt').value;
+    ctx.strokeText(bottomText, gUserPrefs.pos, 150);
+    ctx.fillText(bottomText, gUserPrefs.pos, 150);
+}
+
+function alignText(align) {
+    var pos;
+    switch (align) {
+        case 'start':
+            pos = 0;
+            break;
+        case 'end':
+            pos = 300;
+            break;
+        default:
+            pos = 150;
+            break;
+    }
+    gUserPrefs.align = align;
+    gUserPrefs.pos = pos;
+}
+
+function changeFontSize(op) {
+    if (op === '+') {
+        gUserPrefs.fontSize++
+    } else {
+        gUserPrefs.fontSize--
+    }
+}
+
+function changeTextColor() {
+    var color = document.querySelector('.fill-color').value;
+    gUserPrefs.fillColor = color;
+    renderCanvas()
+}
+
+function toggleShadow() {
+    if (gUserPrefs.shadowBlur === 0) {
+        gUserPrefs.shadowBlur = 5;
+    } else {
+        gUserPrefs.shadowBlur = 0;
+    }
+}
+
+function changeFont() {
+    var font = document.querySelector('.font-family').value;
+    gUserPrefs.font = font;
+    renderCanvas()
+}
+
+function addUserImg() {
+    var elName = document.querySelector('.usersImgName').value
+    var elUrl = document.querySelector('.usersImgUrl').value
+    var newImgObj = { id: gImgObjs.length + 1, name: elName, url: elUrl, keywords: elName }
+    gImgObjs.push(newImgObj)
+    gUserPrefs.imgId = gImgObjs.length;
+    renderImages(gImgObjs)
+    changeStep('step-two', gImgObjs.length)
+}
+
+function deleteText() {
+    var elTopText = document.querySelector('.top-txt');
+    elTopText.value = '';
+    renderCanvas();
+}
+
+function downloadImg(elLink) {
+    var canvas = document.querySelector('canves')
+	var image = new Image();
+	image.src = canvas.toDataURL("image/png");
+	return image;
+}
