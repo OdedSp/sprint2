58c8b142272d593ab3739f039e9b9fbaf8061b95
Oded Spitzer <odedspitzer@gmail.com>
2017-11-20 14:40:11 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@
 console.log('Meme Generator');
 
 var gImgObjs = [
-    { id: 1, name: 'Ancient-Aliens', url: 'img/memes/Ancient-Aliens.jpg', keywords: ['human'] },
-    { id: 2, name: 'Bad-Luck-Brian', url: 'img/memes/Bad-Luck-Brian.jpg', keywords: ['human', 'sweater', 'braces', 'redhead'] },
-    { id: 3, name: 'Business-Cat', url: 'img/memes/Business-Cat.jpg', keywords: ['animal', 'cute'] },
-    { id: 4, name: 'Confession-Bear', url: 'img/memes/Confession-Bear.jpg', keywords: ['animal', 'cute', 'sad'] },
-    { id: 5, name: 'First-World-Problems', url: 'img/memes/First-World-Problems.jpg', keywords: ['human', 'sad', 'cynic', 'sarcastic'] },
-    { id: 6, name: 'Not-Sure-If', url: 'img/memes/Not-Sure-If.jpg', keywords: ['cartoon', 'fry', 'futurama'] },
-    { id: 7, name: 'One-Does-Not-Simply', url: 'img/memes/One-Does-Not-Simply.jpg', keywords: ['human', 'sean', 'bean', 'lord of the rings', 'boromir'] },
-    { id: 8, name: 'Third-World-Skeptical-Kid', url: 'img/memes/Third-World-Skeptical-Kid.jpg', keywords: ['problem', 'human', 'black', 'african'] },
-    { id: 9, name: 'X-Everywhere', url: 'img/memes/X-Everywhere.jpg', keywords: ['cartoon', 'buzz', 'woody', 'toy story'] },
-    { id: 10, name: 'Y-U-No', url: 'img/memes/Y-U-No.jpg', keywords: ['cartoon', 'angry', 'rage'] },
+    { id: 1, name: 'Ancient Aliens', url: 'img/memes/Ancient-Aliens.jpg', keywords: ['human'] },
+    { id: 2, name: 'Bad Luck Brian', url: 'img/memes/Bad-Luck-Brian.jpg', keywords: ['human', 'sweater', 'braces', 'redhead'] },
+    { id: 3, name: 'Business Cat', url: 'img/memes/Business-Cat.jpg', keywords: ['animal', 'cute'] },
+    { id: 4, name: 'Confession Bear', url: 'img/memes/Confession-Bear.jpg', keywords: ['animal', 'cute', 'sad'] },
+    { id: 5, name: 'First World Problems', url: 'img/memes/First-World-Problems.jpg', keywords: ['human', 'sad', 'cynic', 'sarcastic'] },
+    { id: 6, name: 'Not Sure If', url: 'img/memes/Not-Sure-If.jpg', keywords: ['cartoon', 'fry', 'futurama'] },
+    { id: 7, name: 'One Does Not Simply', url: 'img/memes/One-Does-Not-Simply.jpg', keywords: ['human', 'sean', 'bean', 'lord of the rings', 'boromir'] },
+    { id: 8, name: 'Third World Skeptical Kid', url: 'img/memes/Third-World-Skeptical-Kid.jpg', keywords: ['problem', 'human', 'black', 'african'] },
+    { id: 9, name: 'X Everywhere', url: 'img/memes/X-Everywhere.jpg', keywords: ['cartoon', 'buzz', 'woody', 'toy story'] },
+    { id: 10, name: 'Y U No', url: 'img/memes/Y-U-No.jpg', keywords: ['cartoon', 'angry', 'rage'] },
 ]
 
 var gKeywords = {};
@@ -32,7 +32,7 @@ function init() {
 //This function takes the name of the img object and iserts it to the keywords
 function expandKeywords() {
     gImgObjs.forEach(function (img) {
-        var nameKeywords = img.name.split('-');
+        var nameKeywords = img.name.split(' ');
         var newKws = nameKeywords.concat(img.keywords);
         newKws = newKws.map(function (keyword) {
             return keyword.toLowerCase();
